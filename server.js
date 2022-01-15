1637a009edd1b15a113ba8b15c963566172e6214
Soufyan Najjar <soufyan.najjar@student.ehb.be>
2022-01-15 21:34:22 +0100

    FIX BUG SERVER.JS (load express & configure EJS)

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// Import express
+let express = require("express");
+
+//load path
+const path = require('path');
+
+// Inititialize the app
+let app = express();
+
+
+//Inititialize & use EJS
+app.set('view engine', 'ejs');
+
+
+// Configure bodyparser to handle POST requests
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
 // Launch server
 var port = 8000;
 app.listen(port, function () {
